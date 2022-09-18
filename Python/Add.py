4939768ada6e1274f270488ea58e333926cfac2d
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-09-18 22:50:08 +0200

    Starter

---
+++ Python/Add.py
@@ -0,0 +1,40 @@
+
+
+def Add_funtion():
+
+    num1 = 0
+    num2 = 0
+
+    print(" __________________________________")
+    print("| Calculator                       |")
+    print("|----------------------------------|")
+    print("|                                  |")
+    print("| Add your first number:           |")
+    print("|__________________________________|")
+
+    num1 = float(input(""))
+
+    print(" __________________________________")
+    print("| Calculator                       |")
+    print("|----------------------------------|")
+    print("|                                  |")
+    print("| Add your second number:          |")
+    print("|__________________________________|")
+
+    num2 = float(input(""))
+    result = num1 + num2
+    num1 = str(num1)
+    num2 = str(num2)
+    result = str(result)
+    print(" _______________________________________")
+    print("| Calculator                            |")
+    print("|---------------------------------------|")
+    print("|                                       |")
+    print("| The result of " + num1 + " and " + num2 + " is " + result +"     |")
+    print("|_______________________________________|")
+
+
+
+Add_funtion()
+
+    
---
+++ Python/Main.py
@@ -0,0 +1,21 @@
+from ast import Add
+
+
+inport Add
+
+print(" __________________________________")
+print("| Calculator                       |")
+print("|----------------------------------|")
+print("|                                  |")
+print("| Select your choice:              |")
+print("|                                  |")
+print("| -1  Add ->                       |")
+print("| -2  Subtract ->                  |")
+print("| -3  multiply ->                  |")
+print("|                                  |")
+print("|__________________________________|")
+
+Awnser = input("")
+
+if(Awnser == 1):
+    
